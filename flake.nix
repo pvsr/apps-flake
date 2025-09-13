ad6e7578e145389d7cee7e7f80e062fbf95c032c
Peter Rice <peterrice@fastmail.com>
2025-09-13 15:02:28 -0400

    add fish

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "Various dev tools I often want to install from source";
 
   inputs = {
+    fish.url = "github:fish-shell/fish-shell";
+    fish.flake = false;
     jj.url = "github:jj-vcs/jj/v0.33.0";
     jj.inputs.nixpkgs.follows = "nixpkgs";
     helix.url = "github:helix-editor/helix";
@@ -9,8 +11,8 @@
     helix.inputs.rust-overlay.follows = "jj/rust-overlay";
 
     gleam-nix.url = "github:vic/gleam-nix";
-    gleam-nix.inputs.nixpkgs.follows = "nixpkgs";
     gleam-nix.inputs.gleam.url = "github:gleam-lang/gleam/v1.12.0";
+    gleam-nix.inputs.nixpkgs.follows = "nixpkgs";
     janet.url = "github:janet-lang/janet/v1.39.1";
     janet.flake = false;
   };
@@ -18,7 +20,12 @@
   outputs =
     { nixpkgs, ... }@inputs:
     let
-      inherit (builtins) attrValues removeAttrs;
+      inherit (builtins)
+        attrValues
+        removeAttrs
+        concatStringsSep
+        replaceStrings
+        ;
       metaPackages = [
         "all"
         "dev"
@@ -33,6 +40,21 @@
     in
     {
       packages = forAllSystems (system: {
+        fish = nixpkgs.legacyPackages.${system}.fish.overrideAttrs (
+          finalAttrs: previousAttrs: {
+            src = inputs.fish;
+            version = inputs.fish.shortRev;
+            cargoDeps = nixpkgs.legacyPackages.${system}.rustPlatform.fetchCargoVendor {
+              inherit (finalAttrs) src patches;
+              hash = "sha256-zXaLTROJ90+Gv8M9B9zIcu9MJdtZskgvMSsms+NNAOc=";
+            };
+            postPatch = concatStringsSep "\n" [
+              "rm tests/checks/version.fish"
+              (replaceStrings [ "src/tests/highlight.rs" ] [ "src/highlight/tests.rs" ] previousAttrs.postPatch)
+            ];
+          }
+        );
+
         jj = inputs.jj.packages.${system}.default;
         helix = inputs.helix.packages.${system}.default;
 
@@ -45,6 +67,7 @@
         dev = nixpkgs.legacyPackages.${system}.buildEnv {
           name = "pvsr/dev-tools";
           paths = with inputs.self.packages.${system}; [
+            fish
             jj
             helix
           ];
