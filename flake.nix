34328bacecc0c4dfff5381c35a0d77d6f506defd
Peter Rice <peterrice@fastmail.com>
2025-09-13 15:11:44 -0400

    add gleam via gleam-nix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
     helix.url = "github:helix-editor/helix";
     helix.inputs.nixpkgs.follows = "nixpkgs";
     helix.inputs.rust-overlay.follows = "jj/rust-overlay";
+
+    gleam-nix.url = "github:vic/gleam-nix";
+    gleam-nix.inputs.nixpkgs.follows = "nixpkgs";
+    gleam-nix.inputs.gleam.url = "github:gleam-lang/gleam/v1.12.0";
     janet.url = "github:janet-lang/janet/v1.39.1";
     janet.flake = false;
   };
@@ -26,10 +30,13 @@
       packages = forAllSystems (system: {
         jj = inputs.jj.packages.${system}.default;
         helix = inputs.helix.packages.${system}.default;
+
+        gleam = inputs.gleam-nix.packages.${system}.default;
         janet = nixpkgs.legacyPackages.${system}.janet.overrideAttrs {
           src = inputs.janet;
           version = inputs.janet.shortRev;
         };
+
         default = nixpkgs.legacyPackages.${system}.buildEnv {
           name = "src-apps";
           paths = attrValues (removeAttrs inputs.self.packages.${system} [ "default" ]);
